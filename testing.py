6caf7fbe46a06a2c1b1b5c0351f924121c46c0ce
Nathaniel Dafoe <ndafoe60@gmail.com>
2024-12-05 21:03:03 -0700

    added test files for very simple cases.

--- testing.py
+++ testing.py
@@ -1,5 +1,8 @@
 import argparse
 import re
+import hardConstraints
+import main
+import softConstraints
 
 # --------------------------- Setup ---------------------------
 # Create command line arguments
@@ -17,7 +20,7 @@ inputParser.add_argument("secdiffWeight", type=int)
 inputParser.add_argument("gameminPenalty", type=int)
 inputParser.add_argument("practiceminPenalty", type=int)
 inputParser.add_argument("notpairedPenalty", type=int)
-inputParser.add_argument("sectionPenalty",type=int)
+inputParser.add_argument("sectionPenalty", type=int)
 
 # Parse the arguments
 args = inputParser.parse_args()
@@ -25,6 +28,10 @@ args = inputParser.parse_args()
 
 # --------------------------- Parse ---------------------------
 tables = {} # Using a dictionary, key: headers, values: rows
+            # tables[someHeader] = [rows] <- list of rows
+games = [] # games array
+
+
 
 validHeader = ("Name:", "Game slots:", "Practice slots:", "Games:", "Practices:", "Not compatible:", "Unwanted:", "Preferences:", "Pair:", "Partial assignments:") # a set of strings containing headers
 currentHeader = None
@@ -36,6 +43,37 @@ with open(args.filename, "r") as inputFile:
         if line in validHeader:
             currentHeader = line
             tables[currentHeader] = [] # empty list with key header, list will hold rows
+            continue
+        
+        line = re.sub(r",\s*", ", ", line) # clean up the excess or none spacing after commas
+        
+        # Upon reaching Games: header we will create arrays for games
+        # Game number key
+        # - positive number league/age, young divisions
+        # - positive number plus constant, = evening, young divisions
+        # - negative number for league/ age group 16 and above
+        # - negative number minus constant = evening, old divisions
+        if currentHeader == "Games:":
+            line = line.split()
+            age = line[1][1:2]
+            division = line[3][1:]
+            
+            if (age < 16) and (division < 9):
+                games.append([1, ()])
+                
+            elif (age < 16) and (division >= 9):
+                games.append([1+main.EVENING_CONST, ()])
+                
+            elif (age >= 16) and (division < 9):
+                games.append([-1, ()])
+                
+            elif (age >= 16) and (division >= 9):
+                games.append([-1-main.EVENING_CONST, ()])
+                
+            
+                
+            
+            
             
         elif line:
             line = re.sub(r",\s*", ", ", line) # clean up the excess or none spacing after commas
@@ -45,5 +83,27 @@ with open(args.filename, "r") as inputFile:
             
             tables[currentHeader].append(line)
             
+
+
+
+# --------------------------- Populate Hard Constraints ---------------------------
+for rows in tables["Not compatible:"]:
+    hardConstraints.set_unwanted()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
 # TODO integrate the soft constraint integer modifiers
-        
+
+
