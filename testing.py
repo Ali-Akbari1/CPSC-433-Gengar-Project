defebe4165cd82b4543c5914f9eb678d727112f7
Ryan <122650447+ryanl14@users.noreply.github.com>
2024-12-06 05:54:35 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 import argparse
 import re
-# import hardConstraints
+import hardConstraints
 import main
 import softConstraints
 
@@ -27,89 +27,146 @@ args = inputParser.parse_args()
 
 
 # --------------------------- Parse ---------------------------
-tables = {} # Using a dictionary, key: headers, values: rows
-            # tables[someHeader] = [rows] <- list of rows
-games = [] # games array
-practices = [] # practices array
-
-
-
-validHeader = ("Name:", "Game slots:", "Practice slots:", "Games:", "Practices:", "Not compatible:", "Unwanted:", "Preferences:", "Pair:", "Partial assignments:") # a set of strings containing headers
-currentHeader = None
-
 with open(args.filename, "r") as inputFile:
-    for line in inputFile:
-        line = line.strip() # remove leading or trailing spaces: " "
-        
-        if line in validHeader:
-            if currentHeader != "Games:" or "Practice:":
-                tables[currentHeader] = [] # empty list with key header, list will hold rows
-                _ = None
-            else:
-                tables[currentHeader] = {}
-                _ = None
-            continue
+    
+    
+    tables = {} # Using a dictionary, key: headers, values: rows
+                # tables[someHeader] = [rows] <- list of rows
+    games = [] # games array
+    practices = [] # practices array
+    
+    # slots creation
+    slots = main.init_slots(0, 0, 0, 0)
 
-        # empty line
+    gameCounter = 0
+    practiceCounter = 0
+    validHeader = ("Name:", "Game slots:", "Practice slots:", "Games:", "Practices:", "Not compatible:", "Unwanted:", "Preferences:", "Pair:", "Partial assignments:") # a set of strings containing headers
+    currentHeader = None
+    
+    
+    for line in inputFile:
+        line = line.strip()
+        
         if not line:
             continue
         
+        if line in validHeader:
+            currentHeader = line
+            if (currentHeader == "Games:"):
+                tables[currentHeader] = {} 
+                _ = None
+                
+            elif (currentHeader == "Practices:"):
+                tables[currentHeader] = {}
+                _ = None
+            else:
+                tables[currentHeader] = [] # empty list with key header, list will hold rows
+            continue
+
         line = re.sub(r",\s*", ", ", line) # clean up the excess or none spacing after commas
         
-        # Upon reaching Games: header we will create arrays for games
+        # ####################### Parsing Game Slots: ########################
+        # MO = 0-12, TU = 13-38, WE = 39-51, TH = 52-76, FR = 
+        if currentHeader == "Game slots:":
+            gameLine = line.split(", ")    # [Day (ie MO), time (ie 8:00), gameMax, gameMin]
+            if gameLine[0] == "MO":        # assign to Wednesday and Friday too
+                slotInd = main.get_slot_index('M', gameLine[1])
+                slots[slotInd][0] = int(gameLine[2])    # update the gameMax of specified slot index
+                slots[slotInd][1] = int(gameLine[3]) 
+            else:
+                slotInd = main.get_slot_index('T', gameLine[1])
+                slots[slotInd][0] = int(gameLine[2])    
+                slots[slotInd][1] = int(gameLine[3])
+                
+                
+        # ####################### Parsing Practice Slots: ########################
+        if currentHeader == "Practice slots:":
+            pracLine = line.split(", ")
+            if pracLine[0] == "MO":        # assign to Wednesday and Friday too
+                slotInd = main.get_slot_index('M', pracLine[1])
+                slots[slotInd][2] = int(pracLine[2])    # update the gameMax of specified slot index
+                slots[slotInd][3] = int(pracLine[3]) 
+            elif pracLine[0] == "TU":
+                slotInd = main.get_slot_index('T', pracLine[1])
+                slots[slotInd][2] = int(pracLine[2])    
+                slots[slotInd][3] = int(pracLine[3])
+            else:
+                slotInd = main.get_slot_index('F', pracLine[1])
+                slots[slotInd][2] = int(pracLine[2])    
+                slots[slotInd][3] = int(pracLine[3])
+        
+        # ####################### Parsing Games: ########################
         if currentHeader == "Games:":
-            print("adding game to game array")
             
-            main.addGame(tables, games, line)
+            # 
+            lineCopy = line.split()
+            age = int(lineCopy[1][1:2])     # age/tier
+            division = int(lineCopy[3][1:])
             
-            
-            
-            # lineCopy = line.split()
-            # age = lineCopy[1][1:2]
-            # division = lineCopy[3][1:]
-            
-            # if (age < 16) and (division < 9):
-            #     games.append([1, ()])
-            # elif (age < 16) and (division >= 9):
-            #     games.append([1+main.EVENING_CONST, ()]) 
-            # elif (age >= 16) and (division < 9):
-            #     games.append([-1, ()])
-            # elif (age >= 16) and (division >= 9):
-            #     games.append([-1-main.EVENING_CONST, ()])
+            # HELP: not sure about the logic below
+            if (age < 16) and (division < 9):
+                games.append([1, ()])
+            elif (age < 16) and (division >= 9):
+                games.append([1+main.EVENING_CONST, ()]) 
+            elif (age >= 16) and (division < 9):
+                games.append([-1, ()])
+            elif (age >= 16) and (division >= 9):
+                games.append([-1-main.EVENING_CONST, ()])
                 
             practices.append([])
+            
+            tables["Games:"][line] = gameCounter # for quick referencing the index of game strings, used in practices
+            gameCounter += 1
         
+        # ####################### Parsing Practices: ########################
+        if currentHeader == "Practices:":
+            for i in range(len(line)):
+                subString = line[:i+1]
+                # print(subString)
+                # print(tables["Games:"])
+                if subString in tables["Games:"]:
+                    practices[tables["Games:"][subString]].append(())
+            tables["Practices:"][line] = practiceCounter
+            practiceCounter += 1
+        
+        # ####################### Parsing Not Compatible: ########################
+        elif currentHeader == "Not compatible:":
+            event1, event2 = line.split(", ")
+            if event1 in tables["Games:"]:
+                event1_index = tables["Games:"][event1]
+            elif event1 in tables["Practices:"]:
+                event1_index = (0, 0)                       # HELP: not sure what to put for the practice indices still
                 
-        # elif currentHeader == "Practice:":
-        #     for i in range(len(line)):
-        #         subString = line[:i+1]
-        #         if subString in tables["Games:"]:
-        #             practices[tables["Games:"][subString]].append([])
-                  
-                  
-        # elif currentHeader == "Not compatible:":
-        #     event1, event2 = line.split(", ")
-        #     if event1 in tables["Games:"]:
-        #         event1 = tables["Games:"]
-        #     elif event1 in tables["Practice:"]:
-            
-        #     # for row in tables["Games:"]:
-        #     #     if row == line[0]:
-        #     #         event_index1 = row
-        #     #     if row == line[1]:
-        #     #         event_index2 = row
-                    
-        #     hardConstraints.set_incompatible()
+            if event2 in tables["Games:"]:
+                event2_index = tables["Games:"][event2]
+            elif event2 in tables["Practices:"]:
+                event2_index = (0, 0)                       # ^^^
+                
+            hardConstraints.set_incompatible(event1_index, event2_index)
             
             
-        # create paralelle
+        elif currentHeader == "Unwanted:":
+            lineSplit = line.split()
+            
+            hardConstraints.set_unwanted()
+            
+            
+            
+            
+            
+            
+        # create paralelles
         if line:
             line = re.sub(r",\s*", ", ", line) # clean up the excess or none spacing after commas
             
             # line = re.sub(r"\s+", " ", line) # cleanup possible excess spaces between words
             # ^ maybe unneccesary
-            
-            tables[currentHeader].append(line)
+            if currentHeader == "Games:":
+                continue
+            if currentHeader == "Practices:":
+                continue
+            else:
+                tables[currentHeader].append(line)
             
 
 
